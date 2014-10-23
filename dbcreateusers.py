126ff3fa20ea8cb1d8607cb43e1c1e2c9139be6b
Mike Hobizal <mike@Mikes-MacBook-Pro.local>
2014-10-23 14:51:21 -0700

    dbcreateusers.py

--- dbcreateusers.py
+++ dbcreateusers.py
@@ -3,5 +3,6 @@ from project.models import User
 
 db.session.add(User("mike", "mike@hobiz.al", "nikeboy"))
 db.session.add(User("admin", "admin@admin.com", "admin"))
+db.session.add(User("jon", "jon@admin.com", "jon"))
 
 db.session.commit()
