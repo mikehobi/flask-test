c1ace8ff2d3302335f9858b1932a11d8b67eafd7
Mike <mikehobizal@gmail.com>
2015-01-30 14:16:22 -0800

    users attempting to be greedy awards others

--- project/home/views.py
+++ project/home/views.py
@@ -1,4 +1,5 @@
 import json
+import random
 import requests
 from datetime import date
 
@@ -51,28 +52,37 @@ def response():
 		db.session.commit()
 		return slack.response('you just changed your image, congratz man')
 	available_points = from_user.points_to_give
+	points = int(text[1])
 	if text[0] == 'balance':
 		return slack.response('You have {} left for today.'.format(available_points))
 	to_user = text[0]
 	to_user = db.session.query(User).filter(User.name == to_user).first()
-	if to_user == from_user:
-		payload = {
-	        'text': '{} just unsuccessfully tried to give himself points! Everybody point and laugh!'.format(from_user.name),
-			'channel': '#' + channel
-	    }
-		req = requests.post(webhook_url, data={'payload': json.dumps(payload)})
-		if req.status_code != 200:
-	 		return slack.response('Error: {}'.format(req.content))
-		return slack.response('')
 	if to_user is None:
 		return slack.response('wrong. make sure you do [user] before [points].')
-	points = int(text[1])
 	if points <= 0:
 		return slack.response('chill bro, points is about positivity, man')
 	if available_points == 0:
 		return slack.response('you don\'t have any points left today, like literally zero dude')
 	if available_points - points < 0:
 		return slack.response('you don\'t have enough points! you have {} left for today.'.format(available_points))
+
+	if to_user == from_user:
+		rand = random.randrange(0, db.session.query(User).count()) 
+		rand_user = db.session.query(User)[rand]
+		from_user.points_to_give -= points
+		n = points
+		for i in range(0,n):
+			point = Point(rand_user.id)
+			db.session.add(point)
+		db.session.commit()
+		payload = {
+	        'text': '{} just tried to give himself {} point{}! Instead we\'ll randomly give those points to {}!'.format(from_user.name,points,'' if points == 1 else 's',rand_user.name),
+			'channel': '#' + channel
+	    }
+		req = requests.post(webhook_url, data={'payload': json.dumps(payload)})
+		if req.status_code != 200:
+	 		return slack.response('Error: {}'.format(req.content))
+		return slack.response('')
 	from_user.points_to_give -= points
 	n = points
 	for i in range(0,n):
