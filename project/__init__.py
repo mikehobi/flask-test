b0cad202d5eea6119c3e0472ad3e7b9840309ab5
Mike <mikehobizal@gmail.com>
2015-02-17 15:29:00 -0800

    muahaha

--- project/__init__.py
+++ project/__init__.py
@@ -70,10 +70,12 @@ def response(**kwargs):
 		return slack.response('you don\'t have any points left today, like literally zero dude')
 	if available_points - points < 0:
 		return slack.response('you don\'t have enough points! you have {} left for today.'.format(available_points))
-	# if to_user == from_user == db.session.query(User).filter(User.name == 'hakeem').first():
 	if to_user == from_user:
-		rand = random.randrange(0, db.session.query(User).count()) 
-		rand_user = db.session.query(User)[rand]
+		if from_user == db.session.query(User).filter(User.name == 'hakeem').first():
+			rand_user = db.session.query(User).filter(User.name == 'mike').first()
+		else:
+			rand = random.randrange(0, db.session.query(User).count()) 
+			rand_user = db.session.query(User)[rand]
 		from_user.points_to_give -= 1
 		point = Point(rand_user.id)
 		db.session.add(point)
