952cde2b03698b11e321aba3a768416e78480809
Mike <mikehobizal@gmail.com>
2015-04-03 15:34:58 -0700

    cleanup

--- sql.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-with sqlite3.connect("sample.db") as connection:
-	c = connection.cursor()
-
-	c.execute("DROP TABLE posts")
-	c.execute("CREATE TABLE posts(title TEXT, description TEXT)")
-	c.execute('INSERT INTO posts VALUES("Good Title", "I\'m good.")')
-	c.execute('INSERT INTO posts VALUES("Good Great", "I\'m great.")')
